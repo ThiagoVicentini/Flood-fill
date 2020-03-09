b637c5688bc4875972586432cbcc4c9f12cf379e
Matheus Vieira <matheus.v4200@gmail.com>
2020-03-09 00:42:29 -0300

    Alocação da matriz
    Alocação da matriz e leitura completa do txt

--- main.c
+++ main.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void lerMatriz(FILE *file,int i, int j){
-    allocMatriz(i,j);
-    int value;
-    for(int x =0; x<j;x++){
-        for(int x =0; x<i;x++){
-            fscanf(file,"%d",&value);
-            printf("%d ",value);
-        }
+int** allocMatriz(int i, int j){//aloca dinamicamente uma matriz de dimensoes i,j
+    int** matriz;
+    matriz = (int**)malloc (i * sizeof(int *));
+    for (int m = 0; m < i; m++){
+        matriz[m] = (int*)malloc (j * sizeof (int));
     }
+    return matriz;
 }
 
-void allocMatriz(int i, int j){
-    
+int** lerMatriz(FILE *file,int i, int j){
+    int** matriz = allocMatriz(i,j);//recebe a matriz
+    int value;
+    for(int y =0; y<j;y++){//le a coluna
+        for(int  x=0; x<i;x++){//le a linha
+            fscanf(file,"%d",&value);//le cada valor
+            matriz[x][y] = value;//salva em sua respectiva casa
+            printf("%d ",matriz[x][y]);
+        }
+        printf("\n");
+    }
+    return matriz;//retorna matriz completa
 }
-
 
 int main(){
-    char arquivo[100];//        printf("\n"); vetor que armazena o nome do arquivo
-    int i,j;//dimensoes da matriz
-    FILE *file;// arquivo a ser lido
-    scanf("%s",arquivo);//lê o nome do arquivo
-    file = fopen(arquivo,"r");//abre o arquivo
-    fscanf(file,"%d %d",&i,&j);//lê as dimensões da matriz
-    lerMatriz(file,i,j);//lê a matriz
-    fscanf(file,"%d %d",&i,&j);//lê a posição inicial do algoritimo
+    //Declaraçao de variáveis
+        char arquivo[100];//vetor que armazena o nome do arquivo
+        int i,j;//dimensoes da matriz
+        int posInitX, posInitY;// posicoes iniciais
+        int** matriz;//matriz onde os valores serão armazenados
+        FILE *file;// arquivo a ser lido
+    //=========================
+    //Execução
+        scanf("%s",arquivo);//lê o nome do arquivo
+        file = fopen(arquivo,"r");//abre o arquivo
+        fscanf(file,"%d %d",&i,&j);//lê as dimensões da matriz
+        matriz = lerMatriz(file,i,j);//lê a matriz
+        fscanf(file,"%d %d",&posInitX,&posInitY);//lê a posição inicial do algoritimo
+        printf("%d %d",posInitX,posInitY);
+    //=========================
 }
