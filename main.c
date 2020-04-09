2a153424c0eb1d30776f53bdc07fc616c05f94f5
Thiago Vicentini (Anakin) <60662920+ThiagoVicentini@users.noreply.github.com>
2020-04-09 10:41:56 -0300

    Adicionando função mostra_indices()
    Adicionando função para mostrar os índices dos nodos da borda com complexidade O(n²)

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ int** allocMatriz(int i, int j){//aloca dinamicamente uma matriz de dimensoes i,
 
 int** lerMatriz(FILE *file,int i, int j){
     int** matriz = allocMatriz(i,j);//recebe a matriz
-    int value;
     for(int x = 0; x<i;x++){//le a linha
         for(int  y=0; y<j;y++){//le a coluna
             fscanf(file,"%d",&matriz[x][y]);//le cada valor e salva na matriz
@@ -55,7 +54,14 @@ int floodfill(int** matriz,int i,int j,int i_inicial,int j_inicial,int busca, in
     return cont;
 }
 
-void freeMatriz(int **matriz, int i){//percorre todos os espaÃ§os alocados liberando-os
+void mostra_indices(int** matriz, int i, int j, int i_inicial, int j_inicial, int sub){
+    for(int x = i_inicial; x < i-1; x++)
+        for(int y = j_inicial; y < j-1; y++) //percorre a matriz procurando pelos nodos alterados
+            if(matriz[x][y] == sub)
+                printf("%d %d\n", x, y);
+}
+
+void freeMatriz(int **matriz, int i){//percorre todos os espaços alocados liberando-os
   for(int x = 0; x < i; x++){
     free(matriz[x]);
   }
@@ -63,31 +69,24 @@ void freeMatriz(int **matriz, int i){//percorre todos os espaÃ§os alocados liber
 }
 
 int main(){
-    //DeclaraÃ§ao de variÃ¡veis
+    //Declaraçao de variáveis
         char arquivo[100];//vetor que armazena o nome do arquivo
         int i,j;//dimensoes da matriz
         int posInitI, posInitJ;// posicoes iniciais
-        int** matriz;//matriz onde os valores serÃ£o armazenados
+        int** matriz;//matriz onde os valores serão armazenados
         FILE *file;// arquivo a ser lido
     //=========================
-    //ExecuÃ§Ã£o
-        scanf("%s",arquivo);//lÃª o nome do arquivo
+    //Execução
+        scanf("%s",arquivo);//lê o nome do arquivo
         file = fopen(arquivo,"r");//abre o arquivo
-        fscanf(file,"%d %d",&i,&j);//lÃª as dimensÃµes da matriz
-        matriz = lerMatriz(file,i,j);//lÃª a matriz
-        fscanf(file,"%d %d",&posInitI,&posInitJ);//lÃª a posiÃ§Ã£o inicial do algoritimo
-        int cont = floodfill(matriz,i,j,posInitI,posInitJ,matriz[posInitI][posInitJ],2);
+        fscanf(file,"%d %d",&i,&j);//lê as dimensões da matriz
+        matriz = lerMatriz(file,i,j);//lê a matriz
+        fscanf(file,"%d %d",&posInitI,&posInitJ);//lê a posição inicial do algoritimo
+        int cont = floodfill(matriz,i,j,posInitI,posInitJ,matriz[posInitI][posInitJ],2);//aplica o algoritmo e altera os valores de borda para 2
         printf("Total: %d\n",cont);
-        #ifdef DEBUG
-            for(int x = 0; x<i; x++){
-                for(int y = 0; y<j; y++){
-                    printf("%d ",matriz[x][y]);
-                }
-                printf("\n");
-            }
-        #endif
+        mostra_indices(matriz,i,j,posInitI,posInitJ, 2);//procura pelos nodos alterados e printa-os na tela com complexidade O(n²)
     //=========================
-    //FinalizaÃ§Ã£o
+    //Finalização
         fclose(file);
         freeMatriz(matriz,i);
     //=========================
